5e6ae859259475e42ade46897c2551fec8a9cb28
jhaharishankar <harishankarjha21@gmail.com>
2024-12-13 21:22:00 +0530

    register page issue solved

--- Controllers/FrontController.js
+++ Controllers/FrontController.js
@@ -142,7 +142,7 @@ class FrontController {
             html:
                 "<p>Hii " +
                 name +
-                ',Please click here to <a href="http://localhost:3000/verify?id=' +
+                ',Please click here to <a href="https://admission-portal-project1-ez92.onrender.com/verify?id=' +
                 user_id +
                 '">Verify</a>Your mail</p>.',
         });
@@ -346,7 +346,7 @@ class FrontController {
             html:
                 "<p>Hii " +
                 name +
-                ',Please click here to <a href="http://localhost:3000/reset-password?token=' +
+                ',Please click here to <a href="https://admission-portal-project1-ez92.onrender.com/reset-password?token=' +
                 token +
                 '">Reset</a>Your Password.',
         });
